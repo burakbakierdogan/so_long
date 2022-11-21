2226e4be7fd591610e363af593b86bbb3896cf25
burakbakierdogan <bbe2007@outlook.com>
2022-11-21 23:48:19 +0300

    map is done

--- ft_put_map.c
+++ ft_put_map.c
@@ -6,14 +6,50 @@
 /*   By: berdogan <berdogan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 07:36:47 by berdogan          #+#    #+#             */
-/*   Updated: 2022/11/21 08:57:04 by berdogan         ###   ########.fr       */
+/*   Updated: 2022/11/21 23:43:05 by berdogan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include "mlx.h"
 
+static	void	ft_put_walls_spaces(t_mlx *obj, t_map *map)
+{
+	int	i;
 
+	i = 0;
+	while (i < map -> wall_nbr)
+	{
+		mlx_put_image_to_window(obj -> mlx,obj -> mlx_window,
+			obj -> wall, map ->wall[i][0][0], map -> wall[i][0][1]);
+		i++;
+	}
+	i = 0;
+	while (i < map -> space_nbr)
+	{
+		mlx_put_image_to_window(obj -> mlx, obj -> mlx_window,
+			obj -> space, map -> space[i][0][0], map -> space[i][0][1]);
+		i++;
+	}
+	i = 0;
+	while (i < map -> collectible_nbr)
+	{
+		mlx_put_image_to_window(obj -> mlx, obj -> mlx_window,
+			obj -> collectible, map -> collectible[i][0][0], map -> collectible[i][0][1]);
+		i++;
+	}
+
+}
+
+static	void	ft_create_map(t_mlx *obj, t_map *map)
+{
+	ft_put_walls_spaces(obj, map);
+	mlx_put_image_to_window(obj -> mlx, obj -> mlx_window,
+			obj -> player, map -> player[0], map -> player[1]);
+	mlx_put_image_to_window(obj -> mlx, obj -> mlx_window,
+			obj -> ext, map -> ex[0], map -> ex[1]);
+	mlx_loop(obj -> mlx);
+}
 
 static	void	ft_get_data(t_mlx *obj, t_map *map)
 {
@@ -27,13 +63,13 @@ static	void	ft_get_data(t_mlx *obj, t_map *map)
 		(map -> res[1] * 64), "./so_long");
 	obj -> wall = mlx_xpm_file_to_image(obj -> mlx, "./textures/wall.xpm", &x, &y);
 	obj -> player = mlx_xpm_file_to_image(obj -> mlx, "./textures/player.xpm", &x, &y);
-	obj -> space = mlx_xpm_file_to_image(obj -> mlx, "./textures/space.xpm", &x, &y);
+	obj -> space = mlx_xpm_file_to_image(obj -> mlx, "./textures/ground.xpm", &x, &y);
 	obj -> collectible = mlx_xpm_file_to_image(obj -> mlx, "./textures/collectible.xpm", &x, &y);
-	obj -> ext = mlx_xpm_file_to_image(obj -> mlx, "./textures/ext.xpm", &x, &y);
+	obj -> ext = mlx_xpm_file_to_image(obj -> mlx, "./textures/exit.xpm", &x, &y);
 }
 
 void	ft_put_map(t_map *map, t_mlx *obj)
 {
 	ft_get_data(obj, map);
-
+	ft_create_map(obj, map);
 }
